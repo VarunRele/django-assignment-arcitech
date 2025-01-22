da007a5b3dd4e9640c42503d252be6bc1c60ec25
unknown <rele.varun1@gmail.com>
2025-01-22 09:59:51 +0530

    Url updated

--- cms/urls.py
+++ cms/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ContentListView.as_view(), name='content-list'),
+    path('content', views.ContentListView.as_view(), name='content-list'),
     path('content/<int:pk>/', views.ContentRetriveUpdateDestroyView.as_view())
 ]
